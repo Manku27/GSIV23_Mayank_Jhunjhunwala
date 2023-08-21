89ca06f3f1927f9d63069aaaa7e8ae282a648246
Mayank <jjmayank98@gmail.com>
2023-08-21 22:33:42 +0530

    add design for movies list page (mocked)

--- src/list/MoviesList.tsx
+++ src/list/MoviesList.tsx
@@ -1,3 +1,24 @@
+import {mockList} from './mock'
+import {useState} from 'react'
+import {MovieCard} from './MovieCard'
+import {SearchBar} from './SearchBar'
+import './MoviesList.css'
+
 export const MoviesList = () => {
-    return <div>List</div>
+  const [searchKeyWord, setSearchKeyWord] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyWord(event.target.value)
+  }
+
+  return (
+    <>
+      <SearchBar handleChange={handleChange} searchKeyWord={searchKeyWord} />
+      <div className="container">
+        {mockList.results.map((item) => {
+          return <MovieCard item={item} key={item.id} />
+        })}
+      </div>
+    </>
+  )
 }
